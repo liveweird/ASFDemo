4362e985322ed5ff44ffe0524f8ceb524f0e17fa
Sebastian Gębski <sebastian.gebski@hotmail.com>
2016-03-14 14:31:03 +0000

    Visual from Akka.NET project

---
+++ ASFDemo.Web/Hubs/CellularHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace ASFDemo.Web.Hubs
+{
+    public class CellularHub : Hub
+    {
+        public void RandomWakeUp(int cnt)
+        {
+            var random = new Random((int)DateTime.Now.Ticks);
+            for (var i = 0; i < cnt; i++)
+            {
+                // MvcApplication.Ecosystem.Tell(new WakeUpCellMessage { DimX = random.Next(10), DimY = random.Next(10) });
+            }
+        }
+
+        public void WakeMeUp(int x, int y)
+        {
+            // MvcApplication.Ecosystem.Tell(new WakeUpCellMessage { DimX = x, DimY = y });
+        }
+    }
+}
